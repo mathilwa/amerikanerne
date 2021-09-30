921bb01ce5b428d0739f79181f126b77713dcecc
Mathilde <mathildewaerstad@gmail.com>
2021-09-30 19:11:44 +0200

    Legge til logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './icons/cards.png';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Amerikanerne
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
