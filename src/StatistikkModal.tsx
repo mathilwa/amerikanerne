08a47f81c6e324dc21a908a9f0dbdd24a1714520
Mathilde <mathildewaerstad@gmail.com>
2021-12-24 01:20:58 +0100

    Fått ferdig statistikk

--- src/StatistikkModal.tsx
+++ src/StatistikkModal.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import Modal from './Modal';
-import { Spill, Spillere } from './types/Types';
+import { MeldingForMelder, Runde, Spill, Spillere } from './types/Types';
 import countBy from 'lodash.countby';
 import orderBy from 'lodash.orderby';
+import sum from 'lodash.sum';
 
 interface Props {
     alleSpill: Spill[];
@@ -54,6 +55,56 @@ const StatistikkModal: React.FC<Props> = ({ alleSpill, spillere, visModal, onLuk
 
     const lagmeldemInfoSomListeSortert = orderBy(lagmeldemInfoSomListe, 'antallSpill', 'desc');
 
+    const meldinger = alleSpill
+        .map((spill) => {
+            if (spill.runder) {
+                return Object.keys(spill.runder).reduce((meldingerForRunde, runde) => {
+                    const rundeData: Runde = spill.runder![runde];
+                    if (rundeData.melding.antallStikk) {
+                        const meldingerAlleredeForMelder = meldingerForRunde[rundeData.melder] ?? [];
+                        return {
+                            ...meldingerForRunde,
+                            [rundeData.melder]: [...meldingerAlleredeForMelder, rundeData.melding.antallStikk],
+                        };
+                    }
+
+                    return meldingerForRunde;
+                }, {} as MeldingForMelder);
+            } else {
+                return null;
+            }
+        })
+        .filter((melding) => !!melding);
+
+    const alleMeldinger = meldinger.reduce((alleMeldingerForAlleSpillere, spillereMedMelding) => {
+        if (spillereMedMelding) {
+            return Object.keys(spillereMedMelding).reduce((meldingerForEtSpill, spillerId) => {
+                const alleredeRegistrerteMeldinger =
+                    alleMeldingerForAlleSpillere && alleMeldingerForAlleSpillere[spillerId]
+                        ? alleMeldingerForAlleSpillere[spillerId]
+                        : [0];
+
+                return {
+                    ...meldingerForEtSpill,
+                    [spillerId]: alleredeRegistrerteMeldinger.concat(spillereMedMelding[spillerId]),
+                };
+            }, {} as MeldingForMelder);
+        }
+        return alleMeldingerForAlleSpillere;
+    }, {} as MeldingForMelder);
+
+    const alleMeldingerSomListe = alleMeldinger
+        ? Object.keys(alleMeldinger).map((spillerId) => {
+              const antallMeldinger = alleMeldinger[spillerId].length;
+              const sumAlleMeldinger = antallMeldinger && antallMeldinger > 0 ? sum(alleMeldinger[spillerId]) : 0;
+              return {
+                  id: spillerId,
+                  gjennomsnittsmelding: Math.round((sumAlleMeldinger / antallMeldinger) * 10) / 10,
+              };
+          })
+        : [];
+
+    const alleMeldingerSortert = orderBy(alleMeldingerSomListe, 'gjennomsnittsmelding', 'desc');
     const statistikkForKunPagaendeSpill = alleSpill.length === 1;
 
     return (
@@ -64,7 +115,7 @@ const StatistikkModal: React.FC<Props> = ({ alleSpill, spillere, visModal, onLuk
                 <div className="statistikkSeksjon">
                     <h2 className="heading2">Flest seire:</h2>
                     {flestSeire.map((seireInfo) => (
-                        <div>
+                        <div key={'seireInfo-' + seireInfo.id}>
                             <span className="statistikkNavn">{spillere[seireInfo.id].navn}</span> -
                             {seireInfo.antallSeire} seire
                         </div>
@@ -75,17 +126,29 @@ const StatistikkModal: React.FC<Props> = ({ alleSpill, spillere, visModal, onLuk
             <div className="statistikkSeksjon">
                 <h2 className="heading2">Flest meldinger:</h2>
                 {flestMeldinger.map((meldingInfo) => (
-                    <div>
+                    <div key={'meldinginfo-' + meldingInfo.id}>
                         <span className="statistikkNavn">{spillere[meldingInfo.id].navn}</span> -{' '}
                         {meldingInfo.antallMeldinger}
                     </div>
                 ))}
             </div>
 
+            {!statistikkForKunPagaendeSpill && (
+                <div className="statistikkSeksjon">
+                    <h2 className="heading2">Høyest snittmelding:</h2>
+                    {alleMeldingerSortert.map((meldingsinfo) => (
+                        <div key={'allemeldingerinfo-' + meldingsinfo.id}>
+                            <span className="statistikkNavn">{spillere[meldingsinfo.id].navn}</span> - Snittmelding
+                            på {meldingsinfo.gjennomsnittsmelding} stikk
+                        </div>
+                    ))}
+                </div>
+            )}
+
             <div className="statistikkSeksjon">
                 <h2 className="heading2">Antall ganger med på lag uten å melde selv:</h2>
                 {lagmeldemInfoSomListeSortert.map((lagmeldemInfo) => (
-                    <div>
+                    <div key={'lagmeldemInfo-' + lagmeldemInfo.id}>
                         <span className="statistikkNavn">{spillere[lagmeldemInfo.id].navn}</span> -{' '}
                         {lagmeldemInfo.antallSpill}
                     </div>
