6ef11cad3dda1e8bd1a2842b5d756b868a5648a7
Mathilde <mathildewaerstad@gmail.com>
2022-02-19 09:45:21 +0100

    Fikse styling

--- src/gi-poeng/KlarteLagetDet.tsx
+++ src/gi-poeng/KlarteLagetDet.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './gi-poeng.css';
 import { Runde, Spillere } from '../types/Types';
+import SeksjonHeading from '../headings/SeksjonHeading';
 
 interface Props {
     klarteLagetDet: boolean | null;
@@ -9,32 +10,43 @@ interface Props {
     spillere: Spillere;
 }
 
-const GiPoengForRundeModal: React.FC<Props> = ({ klarteLagetDet, onOppdaterKlarteLagetDet, gjeldendeRunde }) => (
-    <>
-        {gjeldendeRunde && gjeldendeRunde.lag && (
-            <>
-                <div className="klarteLagetDetInput">
-                    <label className={`radio ${klarteLagetDet === true ? 'checked' : ''}`}>
-                        <input
-                            type="radio"
-                            onChange={() => onOppdaterKlarteLagetDet(true)}
-                            checked={klarteLagetDet === true}
-                        />
-                        Ja
-                    </label>
+const GiPoengForRundeModal: React.FC<Props> = ({
+    klarteLagetDet,
+    onOppdaterKlarteLagetDet,
+    gjeldendeRunde,
+    spillere,
+}) => {
+    const klarteLagetDetHeading = `Klarte ${spillere[gjeldendeRunde.lag[0]].navn} og ${
+        spillere[gjeldendeRunde.lag[1]].navn
+    } det?`;
+    return (
+        <>
+            {gjeldendeRunde && gjeldendeRunde.lag && (
+                <>
+                    <SeksjonHeading heading={klarteLagetDetHeading} />
+                    <div className="klarteLagetDetInput">
+                        <label className={`radio ${klarteLagetDet === true ? 'checked' : ''}`}>
+                            <input
+                                type="radio"
+                                onChange={() => onOppdaterKlarteLagetDet(true)}
+                                checked={klarteLagetDet === true}
+                            />
+                            Ja
+                        </label>
 
-                    <label className={`radio ${klarteLagetDet === false ? 'checked' : ''}`}>
-                        <input
-                            type="radio"
-                            onChange={() => onOppdaterKlarteLagetDet(false)}
-                            checked={klarteLagetDet === false}
-                        />
-                        Nei
-                    </label>
-                </div>
-            </>
-        )}
-    </>
-);
+                        <label className={`radio ${klarteLagetDet === false ? 'checked' : ''}`}>
+                            <input
+                                type="radio"
+                                onChange={() => onOppdaterKlarteLagetDet(false)}
+                                checked={klarteLagetDet === false}
+                            />
+                            Nei
+                        </label>
+                    </div>
+                </>
+            )}
+        </>
+    );
+};
 
 export default GiPoengForRundeModal;
